75cf7db94c907aa9549117202596de8c25b1c337
igakulesza <73847556+igakulesza@users.noreply.github.com>
2022-06-05 19:58:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,24 +2,23 @@ import requests
 import csv
 
 import sqlite3
-if __name__ == '__main__':
-    file = input()
 
-    open('polaczenia_duze.csv', 'r')
+if _name_ == '_main_':
+    file = input()
     con = sqlite3.connect(":memory:", detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
     cursor = con.cursor()
 
     cursor.execute('''CREATE TABLE IF NOT EXISTS polaczenia_duze (
     from_subscriber INTEGER, to_subscriber INTEGER,  datetime TIMESTAMP, duration INTEGER, celltower INTEGER);''')
-    with open('polaczenia_duze.csv', 'r') as a_file:
-        reader = csv.reader(a_file, delimiter=",")
+    with open(file, 'r') as a_file:
+        reader = csv.reader(a_file, delimiter=";")
         next(reader, None)
-        rows = [row for row in reader]
+        rows = [rows for rows in reader]
         cursor.executemany("INSERT INTO polaczenia_duze VALUES (?, ?, ?, ?, ?)", rows)
-
+        con.commit()
     query2 = 'select sum(duration) from polaczenia_duze'
 
     cursor.execute(query2)
 
-    for i in cursor:
-        print(i)
+    result = cursor.fetchone()[0]
+    print(result)
