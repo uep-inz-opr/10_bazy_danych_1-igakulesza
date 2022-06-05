81dac4b867c418528d44a3040d42e23ae2f814ab
igakulesza <73847556+igakulesza@users.noreply.github.com>
2022-06-05 19:35:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
-import pymysql
+import requests
 import csv
-import datetime
+import sqlite3
 
-plik_csv = open('polaczenia_duze.csv', 'r')
-czytaj = csv.reader(plik_csv,delimiter='|')
+if _name_ == "_main_":
+    file=input()
 
-connection = pymysql.connect(host='ego.kie.ue.poznan.pl', port = 8080, user='inz_opr', passwd='inz_opr', database='inzynieria_oprogramowania')
-cursor = connection.cursor()
+    open('file.csv', 'r')
+    con = sqlite3.connect(":memory:", detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
+    cursor = con.cursor()
 
-for i, rekord in enumerate(czytaj):
-    if i == 0:
-        pass
-    else:
-        from_user = int(rekord[0])
-        to_user = int(rekord[1])
-        duration = int(rekord[3])
-        cell_tower = int(rekord[4])
-        year, month, day, hour, minute = [rekord[2][0:4], rekord[2][4:6], rekord[2][6:8], rekord[2][9:11], rekord[2][11:]]
-        connection_datetime =  '-'.join([year, month, day]) + ' ' + ':'.join([hour, minute, '00'])
-        query = 'insert into polaczenia(`from` , `to` , `datetime` , `duration` , `celltower`) values (%s, %s, %s, %s, %s)'
-        cursor.execute(query, (from_user, to_user, connection_datetime, duration, cell_tower))
-                       
-connection.commit()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS polaczenia_duze (
+    from_subscriber INTEGER, to_subscriber INTEGER,  datetime TIMESTAMP, duration INTEGER, celltower INTEGER);''')
+    with open(file, 'r') as a_file:
+        reader = csv.reader(a_file, delimiter=",")
+        next(reader, None)
+        rows = [row for row in reader]
+        cursor.executemany("INSERT INTO polaczenia_duze VALUES (?, ?, ?, ?, ?)", rows)
+    
+    
+    query2 = 'select sum(duration) from polaczenia_duze'
+    
+    cursor.execute(query2)
+    
+    for i in cursor:
+        print(i)
